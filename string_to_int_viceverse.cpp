6a84e2572fe509a287f3f734c58416956cfdc8a4
Rahul Kapoor <rahul253801@gmail.com>
2022-02-16 00:06:54 +0530

    base conversion b1 to b2

--- string_to_int_viceverse.cpp
+++ string_to_int_viceverse.cpp
@@ -2,56 +2,54 @@
 
 using namespace std;
 
-int string_to_int(string str) {
-	//stoi()
+string convertBase(string str, int base1, int base2) {
 
-	bool isNegative = false;
-	int i = 0;
-	int res = 0;
-
-	if (str[0] == '-') {
-		isNegative = true;
-		i++;
-	}
-
-	while (i < str.length()) {
-
-		int num = str[i] - '0';
-		res = res * 10 + num;
-		i++;
-
-	}
-
-	if (isNegative) {
-		res = res * -1;
-	}
-
-	return res;
-}
-
-string int_to_string(int val) {
-	//to_string()
-	bool isNegative = false;
-
-	if (val == 0) {
-		return "0";
-	}
-
-	if (val < 0) {
-		isNegative = true;
-		val = val * -1;
-	}
-
+	int num = 0;
 	string res = "";
-	while (val > 0) {
-
-		res.push_back(val % 10 + '0');
-		val = val / 10;
-
+	//check if base1 is already a base10
+	if (base1 == 10) {
+		num = stoi(str);
 	}
 
-	if (isNegative) {
-		res.push_back('-');
+	//convert to base10
+	int i = 0;
+	int len = str.length();
+	int x;
+
+	while (i < len) {
+
+		if (str[i] >= 'A' && str[i] <= 'Z') {
+			x = 10 + (str[i] - 'A');
+		}
+		else {
+			x = str[i] - '0';
+		}
+
+		num += x * (pow(base1, len - 1 - i));
+		i++;
+	}
+
+	if (base2 == 10) {
+		res = to_string(num);
+		return res;
+	}
+
+//conversion to base10 is * and +
+//conversion from base10 is % and /
+
+	while (num > 0) {
+
+		int val = num % base2;
+
+		if (val >= 10) {
+			res.push_back('A' + val % 10);
+		}
+		else {
+			res.push_back(val + '0');
+		}
+
+		num = num / base2;
+
 	}
 
 	reverse(res.begin(), res.end());
@@ -66,14 +64,19 @@ int main() {
 	freopen("output.txt", "w", stdout);
 
 	string str;
+	int base1, base2;
+
 	getline(cin, str);
+	cin >> base1;
+	cin >> base2;
 
-	cout << string_to_int(str) << endl;
+	//common bases are:
+	//2 -> 0,1
+	//8 -> 0-7
+	//10 -> 0-9
+	//16 -> hex -> 0-9, A-F
 
-	int val;
-	cin >> val;
-
-	cout << int_to_string(val) << endl;
+	cout << convertBase(str, base1, base2);
 
 	return 0;
 }
