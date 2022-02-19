6ba2745716ad253a20f0dc75390c2761b12bc6ea
Rahul Kapoor <rahul253801@gmail.com>
2022-02-20 00:10:21 +0530

    merge 2 sorted linkedlists handled edge cases and implemented in-place solution

--- merge_sorted_linkedlists.cpp
+++ merge_sorted_linkedlists.cpp
@@ -24,6 +24,21 @@ void computeMerging(Node* head1, Node* head2, Node* &head3) {
 	Node* ptr2 = head2;
 	Node* ptr3 = head3;
 
+	//edge cases
+	if (ptr1 == NULL && ptr2 == NULL) {
+		return;
+	}
+
+	if (ptr1 == NULL) {
+		head3 = ptr2;
+		return;
+	}
+
+	if (ptr2 == NULL) {
+		head3 = ptr1;
+		return;
+	}
+
 	while (ptr1 != NULL && ptr2 != NULL) {
 
 		Node* new_node = new Node();
@@ -83,6 +98,75 @@ void printList(struct Node* head) {
 
 }
 
+struct Node* computeMergingInPlace(Node* head1, Node* head2) {
+
+	Node* ptr1 = head1;
+	Node* ptr2 = head2;
+	Node* ptr3 = NULL;
+	struct Node* temp = NULL;
+	struct Node* head = NULL;
+
+	//edge cases
+	if (ptr1 == NULL && ptr2 == NULL) {
+		return ptr3;
+	}
+
+	if (ptr1 == NULL) {
+		return head2;
+	}
+
+	if (ptr2 == NULL) {
+		return head1;
+	}
+
+	while (ptr1 != NULL && ptr2 != NULL) {
+
+		if (ptr1->data <= ptr2->data) {
+
+			if (ptr3 == NULL) {
+				head = ptr1;
+				ptr3 = head;
+				ptr1 = ptr1->next;
+			}
+			else {
+				temp = ptr1;
+				ptr1 = ptr1->next;
+				ptr3->next = temp;
+				ptr3 = ptr3->next;
+			}
+
+		}
+		else {
+
+			if (ptr3 == NULL) {
+				head = ptr2;
+				ptr3 = head;
+				ptr2 = ptr2->next;
+			}
+			else {
+				temp = ptr2;
+				ptr2 = ptr2->next;
+				ptr3->next = temp;
+				ptr3 = ptr3->next;
+			}
+
+		}
+
+	}
+
+	if (ptr1 == NULL && ptr2 != NULL) {
+		//traverse the remaining ptr2
+		ptr3->next = ptr2;
+	}
+
+	if (ptr2 == NULL && ptr1 != NULL) {
+		//traverse remaining ptr1
+		ptr3->next = ptr1;
+	}
+
+	return head;
+}
+
 int main() {
 	// for getting input from input.txt
 	freopen("input.txt", "r", stdin);
@@ -110,7 +194,14 @@ int main() {
 
 	computeMerging(head1, head2, head3);
 
+	cout << "printing list:" << endl;
 	printList(head3);
+
+	cout << endl;
+	cout << "printing list in-place:" << endl;
+	Node* temp = computeMergingInPlace(head1, head2);
+
+	printList(temp);
 
 	return 0;
 }
