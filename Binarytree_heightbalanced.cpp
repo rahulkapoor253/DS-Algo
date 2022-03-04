f125d50cd5d1c4f1e5ac54069fe411b01f3f4c5d
Rahul Kapoor <rahul253801@gmail.com>
2022-03-04 23:29:26 +0530

    binary tree height balanced optimized solution

--- Binarytree_heightbalanced.cpp
+++ Binarytree_heightbalanced.cpp
@@ -52,6 +52,35 @@ bool isBalanced(struct Node* root) {
 	return false;
 }
 
+//optimized approach can be to calculate height in the same recursive call to find balancing
+bool isBalancedOptimized(struct Node* root, int &height) {
+
+	bool lst = false;
+	bool rst = false;
+
+	int left_height = 0;
+	int right_height = 0;
+
+//we use Post Order Traversal - LEFT, RIGHT, ROOT
+	//height is 0 for a NULL node, but it is height balanced
+	if (root == NULL) {
+		height = 0;
+		return true;
+	}
+
+	lst = isBalancedOptimized(root->left, left_height);
+	rst = isBalancedOptimized(root->right, right_height);
+
+	height = 1 + max(left_height, right_height);
+
+	//height balancing logic
+	if ( abs(left_height - right_height) >= 2 ) {
+		return false;
+	}
+
+	return lst && rst;
+}
+
 int main() {
 	// for getting input from input.txt
 	freopen("input.txt", "r", stdin);
@@ -66,6 +95,14 @@ int main() {
 	root->left->left->left = newNode(8);
 
 	if (isBalanced(root))
+		cout << "Tree is balanced";
+	else
+		cout << "Tree is not balanced";
+
+	cout << endl;
+
+	int height = 0;
+	if (isBalancedOptimized(root, height))
 		cout << "Tree is balanced";
 	else
 		cout << "Tree is not balanced";
